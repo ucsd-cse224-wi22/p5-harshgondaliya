458faca6806d623ff04eb87d40ccfc2f90af5be3
harshgondaliya <harshgondaliya99@gmail.com>
2022-03-11 04:04:43 -0800

    draft3

--- pkg/surfstore/RaftSurfstoreServer.go
+++ pkg/surfstore/RaftSurfstoreServer.go
@@ -2,7 +2,9 @@ package surfstore
 
 import (
 	context "context"
-	"fmt"
+	"log"
+
+	// "fmt"
 	// "fmt"
 	"math"
 	"strings"
@@ -97,16 +99,16 @@ func (s *RaftSurfstore) GetBlockStoreAddr(ctx context.Context, empty *emptypb.Em
 					continue
 				}
 				func (){
-					// fmt.Println("entered internal function")
+					// log.Println("entered internal function")
 					conn, _ := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-					// fmt.Println("conn created")
+					// log.Println("conn created")
 					client := NewRaftSurfstoreClient(conn)
-					// fmt.Println("client created")
+					// log.Println("client created")
 					ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-					// fmt.Println("ctc created")
+					// log.Println("ctc created")
 					defer cancel() // TO DO: what log to send
 					_, err := client.AppendEntries(ctx, &AppendEntryInput{Term: s.term, PrevLogIndex: -1, PrevLogTerm: -1, Entries: []*UpdateOperation{}, LeaderCommit: s.commitIndex})
-					// fmt.Println("cleared appendEntries")
+					// log.Println("cleared appendEntries")
 					if err!=nil && strings.Contains(err.Error(), STR_SERVER_CRASHED){
 						conn.Close()
 						return
@@ -114,7 +116,7 @@ func (s *RaftSurfstore) GetBlockStoreAddr(ctx context.Context, empty *emptypb.Em
 					if err == nil{
 						totalAlive = totalAlive + 1 
 					}
-					// fmt.Println("exited internal function")
+					// log.Println("exited internal function")
 				}()
 				if totalAlive > len(s.ipList)/2{
 					majorityAlive = true
@@ -127,7 +129,7 @@ func (s *RaftSurfstore) GetBlockStoreAddr(ctx context.Context, empty *emptypb.Em
 }
 
 func (s *RaftSurfstore) UpdateFile(ctx context.Context, filemeta *FileMetaData) (*Version, error) {
-	// fmt.Println("Entered Update File")
+	log.Printf("Server%v: Entered Update File\n", s.serverId)
 	if s.isCrashed{
 		return nil, ERR_SERVER_CRASHED
 	}
@@ -145,29 +147,32 @@ func (s *RaftSurfstore) UpdateFile(ctx context.Context, filemeta *FileMetaData) 
 
     success := <-replicated // even heartbeat can send value here
 	if success { // block until successful
-		// fmt.Println("Received success in replication")
+		log.Printf("Server%v: Successful in UpdateFile\n", s.serverId)
+		// log.Println("Received success in replication")
 		return s.metaStore.UpdateFile(ctx, filemeta)
     } 
 	return nil, nil
 }
 
 func (s *RaftSurfstore) AttemptReplication(){ // returns true when replication equal to leaders log is done at majority of servers
-	var targetIdx int64
-	var heartbeat bool
-	if int(s.commitIndex+1) > len(s.log)-1{
-		heartbeat = true
-		targetIdx = int64(len(s.log)-1)
-	} else {
-		heartbeat = false
-		targetIdx = s.commitIndex+1
-	}
-	// fmt.Printf("Entered Attempt Replication with targetId %d\n", targetIdx)
+	targetIdx := s.commitIndex+1
+	log.Printf("Server%v: AttemptReplication with targetIdx=%v\n", s.serverId,targetIdx)
+	// var targetIdx int64
+	// var heartbeat bool
+	// if int(s.commitIndex+1) > len(s.log)-1{
+	// 	heartbeat = true
+	// 	targetIdx = int64(len(s.log)-1)
+	// } else {
+	// 	heartbeat = false
+	// 	targetIdx = s.commitIndex+1
+	// }
+	// log.Printf("Entered Attempt Replication with targetId %d\n", targetIdx)
 	replicateChan := make(chan *AppendEntryOutput, len(s.ipList))
     for idx, _ := range s.ipList {
         if int64(idx) == s.serverId {
             continue
         }
-        go s.Replicate(int64(idx), targetIdx, replicateChan, heartbeat)
+        go s.Replicate(int64(idx), targetIdx, replicateChan)
     }
     replicateCount := 1
 	nodesProbed := 1
@@ -177,27 +182,23 @@ func (s *RaftSurfstore) AttemptReplication(){ // returns true when replication e
 		}
 		rep := <-replicateChan
 		if rep!=nil && rep.Success{
-			// fmt.Println("successful replica")
 			replicateCount++
 			nodesProbed++
-		} else { 
-			// fmt.Println("successful probe")
-			nodesProbed++ } // if crashed then also return to channel
-		if replicateCount > len(s.ipList)/2{
-			if(s.commitIndex < targetIdx){
-				fmt.Printf("Reached Majority for %d and sending true. TargetId: %d, commitIndex: %d\n", s.serverId, targetIdx, s.commitIndex)
-				if(len(s.pendingReplicas)>int(targetIdx)){
-					s.pendingReplicas[targetIdx] <- true // no need to signal second time otherwise will block
-				}
-				s.commitIndex = targetIdx // dont break so that we can reuse for heartbeat; updatefile is already signalled	
-			}
-		}
+		} else { nodesProbed++ } // if crashed then also return to channel
+		if replicateCount > len(s.ipList)/2{ 
+			log.Printf("Server%v: Received Majority Replica with targetIdx=%v\n", s.serverId,targetIdx)
+			s.pendingReplicas[targetIdx] <- true // no need to signal second time otherwise will block
+			s.commitIndex = targetIdx // dont break so that we can reuse for heartbeat; updatefile is already signalled	
+			break
+		}		
 	}
-	// fmt.Println("Exiting Attempt Replication with targetId: ", targetIdx)
+	log.Printf("Server%v: Exiting Attempt Replication\n", s.serverId)
+	// log.Println("Exiting Attempt Replication with targetId: ", targetIdx)
 }
-func (s *RaftSurfstore) Replicate(followerId, entryId int64, replicateChan chan *AppendEntryOutput, isHeartbeat bool) {
+func (s *RaftSurfstore) Replicate(followerId, entryId int64, replicateChan chan *AppendEntryOutput) {
 	for {
-		// fmt.Printf("Entered Replicate with nextId %d and followerId %d\n", s.nextIndex[followerId], followerId)
+		log.Printf("Server%v: Entered Replicate with entryId %v and nextId %v\n", s.serverId, entryId,s.nextIndex[followerId])
+		// log.Printf("Entered Replicate with nextId %d and followerId %d\n", s.nextIndex[followerId], followerId)
 	    addr := s.ipList[followerId]
         conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
@@ -205,70 +206,83 @@ func (s *RaftSurfstore) Replicate(followerId, entryId int64, replicateChan chan 
 		}
 		client := NewRaftSurfstoreClient(conn)
 		var input *AppendEntryInput
-		if s.nextIndex[followerId] == 0{ // either initial stage or reached here after decrementation
-			if len(s.log) != 0{ // something has been updated to the server 
-				input = &AppendEntryInput{ // can be treated as heartbeat without any entries; but for case when log is empty
-					Term: s.term,
-					PrevLogTerm: -1,
-					PrevLogIndex: -1,
-					Entries: s.log[s.nextIndex[followerId]:entryId+1],
-					LeaderCommit: s.commitIndex,
-				}
-			}else {
-				input = &AppendEntryInput{ // can be treated as heartbeat without any entries; but for case when log is empty
-					Term: s.term,
-					PrevLogTerm: -1,
-					PrevLogIndex: -1,
-					Entries: []*UpdateOperation{},
-					LeaderCommit: s.commitIndex,
-				}
+		if int(entryId) >= len(s.log){ // just send a heartbeat to trigger commit
+			input = &AppendEntryInput{
+				Term: s.term,
+				PrevLogTerm: -1,
+				PrevLogIndex: -1,
+				Entries: []*UpdateOperation{}, // nothing to be sent
+				LeaderCommit: s.commitIndex,
 			}
-		} else {
-			if int(s.nextIndex[followerId]) == len(s.log){
-				input = &AppendEntryInput{
-					Term: s.term,
-					PrevLogTerm: s.log[s.nextIndex[followerId]-1].Term,
-					PrevLogIndex: s.nextIndex[followerId]-1,
-					Entries: []*UpdateOperation{}, // nothing to be sent
-					LeaderCommit: s.commitIndex,
-				}	
-			} else {
-				if s.nextIndex[followerId] <= entryId{
-					input = &AppendEntryInput{
+		} else{
+			if s.nextIndex[followerId] == 0{ // either initial stage or reached here after decrementation
+				if len(s.log) != 0{ // something has been updated to the server 
+					input = &AppendEntryInput{ // can be treated as heartbeat without any entries; but for case when log is empty
 						Term: s.term,
-						PrevLogTerm: s.log[s.nextIndex[followerId]-1].Term,
-						PrevLogIndex: s.nextIndex[followerId]-1,
+						PrevLogTerm: -1,
+						PrevLogIndex: -1,
 						Entries: s.log[s.nextIndex[followerId]:entryId+1],
 						LeaderCommit: s.commitIndex,
-					}		
-				} else {
-					input = &AppendEntryInput{
+					}
+				}else {
+					input = &AppendEntryInput{ // can be treated as heartbeat without any entries; but for case when log is empty
 						Term: s.term,
-						PrevLogTerm: s.log[s.nextIndex[followerId]-1].Term,
-						PrevLogIndex: s.nextIndex[followerId]-1,
+						PrevLogTerm: -1,
+						PrevLogIndex: -1,
 						Entries: []*UpdateOperation{},
 						LeaderCommit: s.commitIndex,
 					}
 				}
-			}
+			} else {
+				if int(s.nextIndex[followerId]) == len(s.log){
+					input = &AppendEntryInput{
+						Term: s.term,
+						PrevLogTerm: s.log[s.nextIndex[followerId]-1].Term,
+						PrevLogIndex: s.nextIndex[followerId]-1,
+						Entries: []*UpdateOperation{}, // nothing to be sent
+						LeaderCommit: s.commitIndex,
+					}	
+				} else {
+					if s.nextIndex[followerId] <= entryId{
+						input = &AppendEntryInput{
+							Term: s.term,
+							PrevLogTerm: s.log[s.nextIndex[followerId]-1].Term,
+							PrevLogIndex: s.nextIndex[followerId]-1,
+							Entries: s.log[s.nextIndex[followerId]:entryId+1],
+							LeaderCommit: s.commitIndex,
+						}		
+					} else {
+						input = &AppendEntryInput{
+							Term: s.term,
+							PrevLogTerm: s.log[s.nextIndex[followerId]-1].Term,
+							PrevLogIndex: s.nextIndex[followerId]-1,
+							Entries: []*UpdateOperation{},
+							LeaderCommit: s.commitIndex,
+						}
+					}
+				}
+			}	
 		}
-
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
+		log.Printf("Server%v: Sending input:\n", s.serverId)
+		log.Println(input)
 		output, err := client.AppendEntries(ctx, input)
 		if (output != nil) && (output.Term > s.term){
 			s.term = output.Term
 			s.isLeader = false
 		}
 		if output != nil && output.Success {
+			log.Printf("Server%v: Received success from AppendEntry\n", s.serverId)
 			s.nextIndex[followerId] = output.MatchedIndex+1
 			replicateChan <- output
 			conn.Close()
 			return
 		}
 		if output != nil && !output.Success {
+			log.Printf("Server%v: Received failure from AppendEntry\n", s.serverId)
 			s.nextIndex[followerId]--
-			if s.nextIndex[followerId] < 0{
+			if s.nextIndex[followerId] < 0{ // I dont think we wlil ever enter here
 				s.nextIndex[followerId] = 0 // this basically means i need to send him everything 
 				replicateChan <- &AppendEntryOutput{ServerId: -1, Term: s.term, Success: false, MatchedIndex: -1}
 				conn.Close()
@@ -297,15 +311,18 @@ func (s *RaftSurfstore) Replicate(followerId, entryId int64, replicateChan chan 
 //5. If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index
 //of last new entry)
 func (s *RaftSurfstore) AppendEntries(ctx context.Context, input *AppendEntryInput) (*AppendEntryOutput, error) {
-	// fmt.Printf("Entered AppendEntries\n")
+	log.Printf("Server%v: Entered AppendEntry\n", s.serverId)
+	// log.Printf("Entered AppendEntries\n")
 	output := &AppendEntryOutput{ServerId: s.serverId, Term: s.term, Success: false, MatchedIndex: -1}
 	falseResponse := false
 	if s.isCrashed{
-		// fmt.Println("AppendEntries: crashed server")
+		// log.Println("AppendEntries: crashed server")
+		log.Printf("Server%v: AppendEntry Returned ERR_SERVER_CRASHED \n", s.serverId)
 		return nil, ERR_SERVER_CRASHED
 	}
 	if (input.Term < s.term) { // TODO; revisit - is it safe to return -1
-		// fmt.Println("AppendEntries: stale entry")
+		log.Printf("Server%v: AppendEntry Returned Stale Entry\n", s.serverId)
+		// log.Println("AppendEntries: stale entry")
 		return output, nil
 	} // matchedIndex to be implemented
 	if input.Term > s.term{
@@ -316,34 +333,40 @@ func (s *RaftSurfstore) AppendEntries(ctx context.Context, input *AppendEntryInp
 		falseResponse = true 
 	}// if prevLogTerm is -1 no need to make below check because entire log needs to be replaced
 	if input.PrevLogTerm != -1{
-		// fmt.Println("AppendEntries: prev term not matching")
+		log.Printf("Server%v: AppendEntry Inconsistency\n", s.serverId)
+		// log.Println("AppendEntries: prev term not matching")
 		if !(((len(s.log)-1) >= int(input.PrevLogIndex)) && (s.log[input.PrevLogIndex].Term == input.PrevLogTerm)){
 			falseResponse = true	
 		}	
 	}
-	// fmt.Println("entered append operation")
+	// log.Println("entered append operation")
 	if !falseResponse{
+		log.Printf("Server%v: AppendEntry Entry Appended\n", s.serverId)
 		s.log = append(s.log[:input.PrevLogIndex+1], input.Entries...)
 		output.Success = true
+		output.MatchedIndex = int64(len(s.log)-1)
 	}
-	// fmt.Println("exited append operation")
+	// log.Println("exited append operation")
 	// if input.LeaderCommit > s.commitIndex {
-	s.commitIndex = int64(math.Min(float64(input.LeaderCommit), float64(len(s.log)-1)))
-	for s.lastApplied < s.commitIndex{ // no need to check for updateFile errors because leader would have commited only if there are no errors
-		s.lastApplied++
-		entry := s.log[s.lastApplied]
-		s.metaStore.UpdateFile(ctx, entry.FileMetaData)
-	}
+		log.Printf("Server%v: AppendEntry had LeaderCommit %v and lastLogIndex %v\n", s.serverId, input.LeaderCommit, len(s.log)-1)
+		s.commitIndex = int64(math.Min(float64(input.LeaderCommit), float64(len(s.log)-1)))
+		for s.lastApplied < s.commitIndex{ // no need to check for updateFile errors because leader would have commited only if there are no errors
+			s.lastApplied++
+			entry := s.log[s.lastApplied]
+			s.metaStore.UpdateFile(ctx, entry.FileMetaData)
+		}
+		log.Printf("Server%v: AppendEntry set commitIndex as %v\n", s.serverId, s.commitIndex)
 	// }
-	// fmt.Printf("Exiting AppendEntries\n")
+	// log.Printf("Exiting AppendEntries\n")
 	return output, nil
 }
 
 // This should set the leader status and any related variables as if the node has just won an election
 func (s *RaftSurfstore) SetLeader(ctx context.Context, _ *emptypb.Empty) (*Success, error) {
-	// fmt.Printf("Entered SetLeader\n")
+	log.Printf("Server%v: Entered SetLeader\n",s.serverId)
 	if s.isCrashed {
-		// fmt.Printf("Exiting SetLeader\n")
+		log.Printf("Server%v: Exited SetLeader\n",s.serverId)
+		// log.Printf("Exiting SetLeader\n")
 		return &Success{Flag: false}, ERR_SERVER_CRASHED
 	} else {
 		s.term++
@@ -351,24 +374,26 @@ func (s *RaftSurfstore) SetLeader(ctx context.Context, _ *emptypb.Empty) (*Succe
 		for i:=0; i<len(s.ipList); i++{
 			s.nextIndex[i] = int64(len(s.log))
 		}
-		// fmt.Printf("Exiting SetLeader\n")
+		log.Printf("Server%v: Exited SetLeader\n",s.serverId)
+		// log.Printf("Exiting SetLeader\n")
 		return &Success{Flag: true}, nil
 	}
-	
 }
 
 // Send a 'Heartbeat" (AppendEntries with no log entries) to the other servers
 // Only leaders send heartbeats, if the node is not the leader you can return Success = false
 func (s *RaftSurfstore) SendHeartbeat(ctx context.Context, _ *emptypb.Empty) (*Success, error) {
-	// fmt.Printf("Entering SendHeartBeat\n")
+	// log.Printf("Entering SendHeartBeat\n")
+	log.Printf("Server%v: Entered SendHeartBeat\n",s.serverId)
 	if s.isCrashed{
 		return &Success{Flag: false}, ERR_SERVER_CRASHED
 	}
 	if !s.isLeader{
 		return &Success{Flag: false}, nil
 	}
-	go s.AttemptReplication()
-	// fmt.Printf("Exiting SendHeartBeat\n")
+	s.AttemptReplication()
+	// log.Printf("Exiting SendHeartBeat\n")
+	log.Printf("Server%v: Exited SendHeartBeat\n",s.serverId)
 	return &Success{Flag: true}, nil
 }
 
